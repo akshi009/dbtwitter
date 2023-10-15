bb6de1562c8bc3bea746b8484fbe57deec525c9c
akshi009 <akshi20jain03@gamil.com>
2023-10-15 18:41:48 +0530

    add

--- app.js
+++ app.js
@@ -10,81 +10,196 @@ app.use(cors());
 const PORT = process.env.PORT || 3005;
 app.listen(PORT, () => console.log(`🚀 @ http://localhost:${PORT}`));
 
-// Route for getting all users or specific user by ID
-app.get("/users", async (req, res) => {
+app.get("/post", async (req, res) => {
   try {
-    if (req.query.id) {
-      // If 'id' is provided in the query, return a specific user by ID
-      const user = await prisma.user.findUnique({
-        where: { id: parseInt(req.query.id) },
-      });
-      res.json(user);
-    } else {
-      // If 'id' is not provided, return all users
-      const users = await prisma.user.findMany({});
-      res.json(users);
-    }
-  } catch (error) {
-    console.error("Error:", error);
-    res.status(500).send("Internal Server Eor");
-  }
-});
-
-// Route for creating a new user
-app.post("/users", async (req, res) => {
-  try {
-    const user = await prisma.user.create({
-      data: req.body,
+    const posts = await prisma.post.findMany({
+      include: { user: true },
     });
-    res.status(201).json(user);
-  } catch (error) {
-    console.error("Error:", error);
-    res.status(500).json({ error: "Internal Server Eor" });
-  }
-});
-
-// Route for getting all posts or specific post by ID
-app.get("/posts", async (req, res) => {
-  try {
-    if (req.query.id) {
-      // If 'id' is provided in the query, return a specific post by ID
-      const post = await prisma.post.findUnique({
-        where: { id: parseInt(req.query.id) },
-        include: { user: true },
-      });
-      res.json(post);
-    } else {
-      // If 'id' is not provided, return all posts
-      const posts = await prisma.post.findMany({
-        include: { user: true },
-      });
-      res.json(posts);
-    }
+    res.json(posts);
   } catch (error) {
     console.error("Error:", error);
     res.status(500).send("Internal Server Error");
   }
 });
 
-// Route for creating a new post
-app.post("/posts", async (req, res) => {
+app.get("/user", async (req, res) => {
   try {
+    const users = await prisma.user.findMany({});
+    res.json(users);
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.post("/post", async (req, res) => {
+  
+  try {
+    
     const { userId, caption, image } = req.body;
     if (!userId || !caption) {
       return res.status(400).json({ error: "userId and caption are required fields" });
     }
 
+  
     const post = await prisma.post.create({
       data: {
         userId: userId,
         caption: caption,
-        image: image,
+        image: image, 
       },
     });
 
-    res.status(201).json(post);
+    res.status(201).json(post); 
   } catch (error) {
     console.error("Error creating post:", error);
     res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+
+// app.post("/post", async (req, res) => {
+//   try {
+//     const postDataArray = req.body;
+
+//     if (!Array.isArray(postDataArray)) {
+//       return res.status(400).json({ error: "Request body should be an array of posts" });
+//     }
+
+    
+//     const createdPosts = [];
+
+    
+//     for (const postData of postDataArray) {
+//       const { userId, caption, image } = postData;
+
+//       if (!userId || !caption) {
+//         return res.status(400).json({ error: "userId and caption are required fields for each post" });
+//       }
+
+      
+//       const post = await prisma.post.create({
+//         data: {
+//           user: {
+//             connect: { id: userId }, 
+//           },
+//           caption: caption,
+//           image: image,
+//         },
+//       });
+
+//       createdPosts.push(post);
+//     }
+
+    
+//     res.status(201).json(createdPosts);
+//   } catch (error) {
+//     console.error("Error creating posts:", error);
+//     res.status(500).json({ error: "Internal Server Error" });
+//   }
+// });
+
+
+
+
+app.post("/user", async (req, res) => {
+  try {
+    const user = await prisma.user.create({
+      data: req.body,
+    });
+    res.json(user);
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+
+// app.post("/user", async (req, res) => {
+//   try {
+//     const usersArray = req.body;
+    
+    
+//     const usersData = usersArray.map((user) => ({
+//       name: user.name,
+//       email: user.email,
+//       image: user.image,
+//     }));
+
+//     const usersCreated = await prisma.user.createMany({
+//       data: usersData,
+//     });
+
+//     res.status(204).send(usersCreated);
+//   } catch (e) {
+//     console.error(e.message);
+//     res.status(500).send(e.message);
+//   }
+// });
+
+
+
+
+
+
+
+
+app.delete("/post/:id", async (req, res) => {
+  const id = parseInt(req.params.id);
+  try {
+    const deletedPost = await prisma.post.delete({
+      where: {
+        id: id,
+      },
+    });
+    res.json(deletedPost);
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.delete("/user/:id", async (req, res) => {
+  const id = parseInt(req.params.id);
+  try {
+    const deleteduser = await prisma.user.delete({
+      where: {
+        id: id,
+      },
+    });
+    res.json(deleteduser);
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+
+
+app.patch("/user/:id", async (req, res) => {
+  try {
+    const updatedUser = await prisma.user.update({
+      where: { id: Number(req.params.id) },
+      data: req.body,
+    });
+    res.json(updatedUser);
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+
+
+app.patch("/post/:id", async (req, res) => {
+  try {
+    const updatedPost = await prisma.post.update({
+      where: { id: Number(req.params.id) },
+      data: req.body,
+    });
+    res.json(updatedPost);
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).send("Internal Server Error");
   }
 });
